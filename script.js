6c517de9bb0eb7c3f2394458139abbcd8acf2bf3
Lusi <lucila.a.navarro@gmail.com>
2020-10-22 17:56:44 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const title = document.getElementById("title-id");
+const description = document.getElementById("description");
+
+function setTitle(newTitle) {
+    title.innerText = newTitle;
+}
+
+function setDescription(newDescription) {
+    description.innerText = newDescription;
+}
+
+setTitle("¿Qué debería hacer hoy?");
+setDescription("keonda la weaaaaaaaaaaaaaa");
